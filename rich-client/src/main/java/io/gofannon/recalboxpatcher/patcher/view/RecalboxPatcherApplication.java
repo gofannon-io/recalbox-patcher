607359aba17e2d049d31be219200161a26641ac2
Gwen <gwen.dev@gmail.com>
2017-05-26 01:11:28 +0200

    feat: add engine simulator dialog

--- rich-client/src/main/java/io/gofannon/recalboxpatcher/patcher/view/RecalboxPatcherApplication.java
+++ rich-client/src/main/java/io/gofannon/recalboxpatcher/patcher/view/RecalboxPatcherApplication.java
@@ -17,7 +17,6 @@ package io.gofannon.recalboxpatcher.patcher.view;
 
 
 import io.gofannon.recalboxpatcher.patcher.view.model.UIModel;
-import io.gofannon.recalboxpatcher.patcher.view.utils.NotImplemented;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.geometry.*;
@@ -115,7 +114,28 @@ public class RecalboxPatcherApplication extends Application {
 
 
     private void onSave(ActionEvent event) {
-        NotImplemented.displayNotImplementedDialog();
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Génération du fichier");
+        alert.setHeaderText("Désolé, le générateur de fichier n'est pas encore intégré.");
+
+        String content = new StringBuilder()
+                .append("Fichier d'entrée Recalbox: ").append(model.inputRecalboxFileProperty().getValue()).append("\n")
+                .append("Fichier d'entrée Hyperspin: ").append(model.inputHyperspinFileProperty().getValue()).append("\n")
+                .append("Fichier de sortie Recalbox: ").append(model.outputRecalboxFileProperty().getValue()).append("\n")
+                .append("Chemin des fichiers à télécharger: ").append(model.inputImageDirectoryProperty().getValue()).append("\n")
+                .append("Chemin des images dans le fichier: ").append(model.outputImageDirectoryProperty().getValue()).append("\n")
+                .append("Hauteur des images: ").append(model.heightImageProperty().getValue()).append("\n")
+                .append("Largeur des images: ").append(model.widthImageProperty().getValue()).append("\n")
+                .append("Extension des images: ").append(model.imageExtensionProperty().getValue()).append("\n")
+                .append("Option 'Non trouvée' activée: ").append(model.notFoundOptionProperty().getValue()).append("\n")
+                .append("Option 'Majuscule' activée: ").append(model.upperCaseOptionProperty().getValue()).append("\n")
+                .append("Option 'Nouveau Fichier' activée: ").append(model.newFileOptionProperty().getValue()).append("\n")
+                .append("Option 'Ajout de nom' activée: ").append(model.addNameOptionProperty().getValue())
+                .toString();
+
+        alert.setContentText(content);
+
+        alert.showAndWait();
     }
 
     private void onExit(ActionEvent event) {
